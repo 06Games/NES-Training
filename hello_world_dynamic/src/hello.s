eeb7dd53db54eb6dc8daa361719d0bcdc8052c76
Evan Galli <evan_g@orange.fr>
2024-07-23 14:31:54 +0200

    Split rendering from main code

---
+++ hello_world_dynamic/src/hello.s
@@ -0,0 +1,45 @@
+nmi:
+	cpy #32 ; 4 palettes times 8 frames of delay
+	bne :+
+	ldy #0
+:	ldx #$00 	; Set SPR-RAM address to 0
+	stx $2003
+	@draw_loop:	
+		lda hello, x ; Get Y coordinate
+		clc
+		adc #$67 ; Center the text by adding 0x67 to the Y coordinate
+		sta $2004 ; Send the value to the PPU-Bus
+		inx
+		lda hello, x ; Get the tile index
+		sta $2004
+		inx
+		tya ; Get the frame index to calculate the palette number
+		lsr a
+		lsr a
+		lsr a ; Change the palette each 8 frames
+		sta $2004
+		lda hello, x ; Get X coordinate
+		clc
+		adc #$6c ; Center the text
+		sta $2004
+		inx
+		cpx #36 ; Hello contains 12 tile definitions of 3 bytes each
+		bne @draw_loop
+	iny
+	rti
+
+hello:
+	;   posY tileI posX
+  .byte 00, $00, 00
+  .byte 00, $00, 00
+  .byte 00, $48, 00
+  .byte 00, $65, 10
+  .byte 00, $6c, 20
+  .byte 00, $6c, 30
+  .byte 00, $6f, 40
+  
+  .byte 10, $57, 00
+  .byte 10, $6f, 10
+  .byte 10, $72, 20
+  .byte 10, $6c, 30
+  .byte 10, $64, 40
--- hello_world_dynamic/src/main.s
+++ hello_world_dynamic/src/main.s
@@ -16,6 +16,8 @@
 ; Main code segment for the program
 .segment "CODE"
 
+.include "./hello.s"
+
 reset:
   sei		; disable IRQs
   cld		; disable decimal mode
@@ -71,52 +73,6 @@ enable_rendering:
   sta $2000
   lda #%00010000	; Enable Sprites
   sta $2001
-
-nmi:
-	cpy #32 ; 4 palettes times 8 frames of delay
-	bne :+
-	ldy #0
-:	ldx #$00 	; Set SPR-RAM address to 0
-  	stx $2003
-	@draw_loop:	
-		lda hello, x ; Get Y coordinate
-		clc
-		adc #$67 ; Center the text by adding 0x67 to the Y coordinate
-		sta $2004 ; Send the value to the PPU-Bus
-		inx
-		lda hello, x ; Get the tile index
-		sta $2004
-		inx
-		tya ; Get the frame index to calculate the palette number
-		lsr a
-		lsr a
-		lsr a ; Change the palette each 8 frames
-		sta $2004
-		lda hello, x ; Get X coordinate
-		clc
-		adc #$6c ; Center the text
-		sta $2004
-		inx
-		cpx #72 ; Hello contains 12 tile definitions of 6 bytes each
-		bne @draw_loop
-	iny
-  	rti
-
-hello:
-	;   posY tileI posX
-  .byte 00, $00, 00
-  .byte 00, $00, 00
-  .byte 00, $48, 00
-  .byte 00, $65, 10
-  .byte 00, $6c, 20
-  .byte 00, $6c, 30
-  .byte 00, $6f, 40
-  
-  .byte 10, $57, 00
-  .byte 10, $6f, 10
-  .byte 10, $72, 20
-  .byte 10, $6c, 30
-  .byte 10, $64, 40
 
 ; Palettes (Background + Sprites)
 palettes:
